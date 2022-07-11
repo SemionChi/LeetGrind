cc302c5b8902eeae470c1f2aef7525b9df5579d1
Semion Tch <94633514+SemionChi@users.noreply.github.com>
2022-07-11 22:43:15 +0300

    Time: 49 ms (90.93%), Space: 14.5 MB (57.36%) - LeetHub

---
+++ 125-valid-palindrome/125-valid-palindrome.py
@@ -0,0 +1,8 @@
+class Solution:
+    def isPalindrome(self, s: str) -> bool:
+        nStr=""
+        for c in s:
+            if c.isalnum():
+                nStr+=c.lower()
+        return nStr==nStr[::-1]
+        
